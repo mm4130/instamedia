229c4478d3d187e17d18ed9a4d4a6601cb8512f2
mm4130 <mm41.ir@gmail.com>
2019-02-25 23:18:00 +0330

    Update usage.php

--- usage.php
+++ usage.php
@@ -1,7 +1,7 @@
 <?php
 include 'functions.php';
 header('Content-Type: application/json; charset=utf-8');
-$url = 
+$url = $_GET['url'];
 $id = insta_id($url); //grabbing insta id from url
 $info = media_info($id); //getting info. Returns array. Check functions.php
 echo json_encode($info,JSON_PRETTY_PRINT); //Printing array as json.
